4f9834d861e08702588c9ce08c4c85589b3d3d25
Mennoj <tieshoenderdos@gmail.com>
2021-04-15 17:07:34 +0200

    een bug gefixed, ipv visibility display

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,5 @@
 function showFunction() {
-    document.querySelector(".hamburger-container").style.visibility = "visible";
+    document.querySelector(".hamburger-container").style.display = "block";
     document.querySelector(".hamburger-container").style.animation = "drop .7s ease";
     document.getElementById("hamburger-knop").style.display = "none"; 
     document.getElementById("hamburger-kruis").style.display = "block";
@@ -8,5 +8,5 @@ function showFunction() {
 function dissapearFunction() {
     document.getElementById("hamburger-kruis").style.display = "none";
     document.getElementById("hamburger-knop").style.display = "block"; 
-    document.querySelector(".hamburger-container").style.visibility = "hidden";
+    document.querySelector(".hamburger-container").style.display = "none";
 }
