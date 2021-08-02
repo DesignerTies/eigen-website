b26e5378c0b48a7d0edf926712d4e5fd3eb705cd
Designerties <tieshoenderdos@gmail.com>
2021-08-02 17:31:44 +0200

    made final changes to the website

--- js/header.js
+++ js/header.js
@@ -9,13 +9,13 @@ window.onscroll = function () {
 function scrollFunction() {
   if (window.pageYOffset >= witNav) {
     document.getElementById("header").style.backgroundColor = "white";
-    document.getElementById("h1-nav").style.color = "black";
+    document.querySelector(".h1-nav").style.color = "black";
     document.getElementById("projecten").style.color = "black";
     document.getElementById("prijzen").style.color = "black";
     document.getElementById("contact").style.color = "black";
   } else {
     document.getElementById("header").style.backgroundColor = "";
-    document.getElementById("h1-nav").style.color = "white";
+    document.querySelector(".h1-nav").style.color = "white";
     document.getElementById("projecten").style.color = "white";
     document.getElementById("prijzen").style.color = "white";
     document.getElementById("contact").style.color = "white";
